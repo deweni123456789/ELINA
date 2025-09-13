6bb16cf8f8376ed29203b3727ea30c97a1f29cda
deweni123456789 <schoford91@gmail.com>
2025-09-13 19:56:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
+# 🔑 Railway / Heroku Environment Variables
 API_ID = int(os.environ.get("5047271"))
 API_HASH = os.environ.get("047d9ed308172e637d4265e1d9ef0c27")
 BOT_TOKEN = os.environ.get("8464050626:AAFjoldNU_A5jHEzSspCDDNUy5__WyEFfms")
+
+# Optional bot name (default = EliZaBeth)
 BOT_NAME = os.environ.get("BOT_NAME", "EliZaBeth")
+
 
